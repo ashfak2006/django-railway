9d10c3c0a6b413a305e31562bdeb046adb6d1d43
Ashafak Ameer <ameerashfak90@gmail.com>
2025-06-26 15:16:00 +0530

    Update settings.py FOR deployment and change database to sql lite

--- online_shope/settings.py
+++ online_shope/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_41x=kh((@kp^y6n836fu2c)8h2(^gg69hn86n)f6+!@_hv8ed'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-45f1.up.railway.app']
 
@@ -80,16 +80,13 @@ WSGI_APPLICATION = 'online_shope.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+ DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'online_shope',
-        'USER': 'postgres',
-        'PASSWORD': 'ashfak123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
